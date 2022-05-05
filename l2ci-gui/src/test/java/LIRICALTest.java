342faa21debd5d63963473c56d7f57ac30933d41
mabeckwith <mbeckwith30@gmail.com>
2022-05-05 16:40:19 -0400

    Start incorporating LIRICAL into L4CI.

---
+++ l2ci-gui/src/test/java/LIRICALTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.monarchinitiative.lirical.configuration.Lirical;
+import org.monarchinitiative.lirical.configuration.LiricalBuilder;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class LIRICALTest {
+//    @Test
+//    public void testRun() throws Exception {
+//        Lirical lirical = LiricalBuilder.builder(Path.of(String.join(File.separator, "src", "main", "resources", "LIRICAL", "data"))).build();
+//    }
+}
